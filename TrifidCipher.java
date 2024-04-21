3c20e5ff9211954fef6ceba4d924bd54429651bb
ermiragashi14 <155022915+ermiragashi14@users.noreply.github.com>
2024-04-21 15:00:57 +0100

    Update TrifidCipher.java

--- TrifidCipher.java
+++ TrifidCipher.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class TrifidCipher {
 
@@ -12,7 +10,6 @@ public class TrifidCipher {
         String inputLine = scanner.nextLine().toUpperCase();
         String[] keywords = inputLine.split("\\s+");
 
-
         StringBuilder keywordBuilder = new StringBuilder();
         for (String keyword : keywords) {
             keywordBuilder.append(keyword);
@@ -21,27 +18,11 @@ public class TrifidCipher {
 
         keyword = keyword.replaceAll("[^A-Z]", "");
 
-
+        // Generate the remaining alphabet without repeating letters from the keyword
         String remainingAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        StringBuilder alphabetBuilder = new StringBuilder();
-        for (char c : remainingAlphabet.toCharArray()) {
-            if (!keyword.contains(String.valueOf(c))) {
-                alphabetBuilder.append(c);
-            }
+        for (char c : keyword.toCharArray()) {
+            remainingAlphabet = remainingAlphabet.replace(String.valueOf(c), "");
         }
-        remainingAlphabet = alphabetBuilder.toString();
-
-
-        ArrayList<Character> alphabetList = new ArrayList<>();
-        for (char c : remainingAlphabet.toCharArray()) {
-            alphabetList.add(c);
-        }
-        Collections.shuffle(alphabetList);
-        remainingAlphabet = "";
-        for (char c : alphabetList) {
-            remainingAlphabet += c;
-        }
-
 
         String alphabet = keyword + remainingAlphabet + "#";
 
@@ -51,7 +32,6 @@ public class TrifidCipher {
 
         char kubi[][][] = new char[a][b][c];
         int index = 0;
-
 
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
@@ -72,28 +52,17 @@ public class TrifidCipher {
             }
         }
 
-
-        // for (int i = 0; i < a; i++) {
-        //     for (int j = 0; j < b; j++) {
-        //         for (int k = 0; k < c; k++) {
-        //             System.out.print(kubi[i][j][k] + " ");
-        //         }
-        //         System.out.println();
-        //     }
-        //     System.out.println();
-        // }
-
+     
         System.out.println("Enter text to encrypt: ");
         String inputText = scanner.nextLine().toUpperCase();
         inputText = inputText.replaceAll("[^A-Z]", "");
-
 
         inputText = inputText.replaceAll("\\s+", "");
 
         char[] text = inputText.toCharArray();
         int[] coordinates = new int[3 * text.length];
 
-
+      
         for (int i = 0; i < text.length; i++) {
             char currentChar = text[i];
             for (int x = 0; x < a; x++) {
@@ -108,6 +77,15 @@ public class TrifidCipher {
                 }
             }
         }
+         // for (int i = 0; i < a; i++) {
+         //     for (int j = 0; j < b; j++) {
+         //         for (int k = 0; k < c; k++) {
+         //             System.out.print(kubi[i][j][k] + " ");
+         //         }
+         //         System.out.println();
+         //     }
+         //     System.out.println();
+         // }
 
 
         char[] cipher = new char[text.length];
@@ -123,7 +101,6 @@ public class TrifidCipher {
         }
     }
 
-    
     private static boolean charExists(char[][][] kubi, char c) {
         for (char[][] slice : kubi) {
             for (char[] row : slice) {
